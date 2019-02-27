c9f193d716ef11032259aaa18b2055d4641719fc
Caroline Setiawan <caroline.setiawan@Carolines-MacBook-Pro.local>
2019-02-27 14:32:40 +1100

    rebased and resolved conflict

--- app/controllers/rating_questions_controller.rb
+++ app/controllers/rating_questions_controller.rb
@@ -8,20 +8,41 @@ class RatingQuestionsController < ApplicationController
   end
 
   def create
-    @rating_question = RatingQuestion.new(question_params)
+    @rating_question = RatingQuestion.new(rating_question_params)
+
     if @rating_question.save
-      # flash[:notice] = "Your question has been created."
-      redirect_to "/", notice: "Your question has been created."
+      redirect_to @rating_question, notice: "Your question has been created."
+    else
+      render 'new'
     end
   end
 
   def edit
     @rating_question = RatingQuestion.find(params[:id])
   end
+  
+  def show
+    @rating_question = RatingQuestion.find(params[:id])
+  end
+
+  def update
+    @rating_questions = rating_questions.find(params[:id])
+  end
+
+  def edit
+    @rating_questions = rating_questions.find(params[:id])
+  end
+
+  def destroy
+    rating_questions = rating_questions.find(params[:id])
+  end
+
+
 
   private
-
-  def question_params
-    params.require(:rating_question).permit(:title)
+  def rating_question_params
+    params.require(:rating_question).permit(:title, :text, :tag)
   end
+  
+
 end
---
+++ spec/requests/show_individual_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+require 'pry'
+
+RSpec.describe "GET /rating_questions/:id" do
+  let(:new_title) { "helo i'm a title" }
+  let(:new_tag) { "i'm a tag" } 
+  let(:question) { RatingQuestion.create! }
+
+  before do
+    get "/rating_questions/#{question.id}.json"
+  end
+
+  context "when the question exists" do
+    it "returns a 200 OK" do
+      expect(response.status).to eq(200)
+    end
+
+    it "returns a question" do
+      expect(JSON.parse(response.body).is_a?(Hash)).to eq(true)
+    end
+  end
+
+  context "asking to get a question that doesn't exist" do
+    before do
+      get("/rating_questions/i-will-never-exist")
+    end
+
+    it "returns a 404 Not Found" do
+      expect(response.status).to eq(404)
+    end
+  end
+
+  
+
+end
