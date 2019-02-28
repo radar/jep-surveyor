1b232f24305fcb1edd7778165350eae0c2ab2bb7
Ryan Bigg <me@ryanbigg.com>
2019-03-01 10:30:07 +1100

    Call .json routes in 03 request spec

--- spec/requests/03_get_single_resource_spec.rb
+++ spec/requests/03_get_single_resource_spec.rb
@@ -1,26 +1,25 @@
 require "rails_helper"
 
-RSpec.describe "GET /rating_questions.json/:id" do
+RSpec.describe "GET /rating_questions/:id.json" do
   context "when the question exists" do
-    it "returns a 200 OK" do
-      post "/rating_questions.json", params: {rating_question: {title: "Hello World"}}
-      question = JSON.parse(response.body)
+    let!(:question) do
+      RatingQuestion.create(title: "Hello World")
+    end
 
-      get("/rating_questions/#{question["id"]}")
+    it "returns a 200 OK" do
+      get("/rating_questions/#{question.id}.json")
       expect(response.status).to eq(200)
     end
 
     it "returns a question" do
-      post "/rating_questions.json", params: {rating_question: {title: "Hello World"}}
-      question = JSON.parse(response.body)
-      get("/rating_questions/#{question["id"]}")
+      get("/rating_questions/#{question.id}.json")
       expect(JSON.parse(response.body).is_a?(Hash)).to eq(true)
     end
   end
 
   context "asking to get a question that doesn't exist" do
     it "returns a 404 Not Found" do
-      get("/rating_questions/i-will-never-exist")
+      get("/rating_questions/i-will-never-exist.json")
       expect(response.status).to eq(404)
     end
   end
