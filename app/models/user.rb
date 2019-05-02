7dd0da246d7878b0c949cb6d3920a7fa86e87765
Kasia <kasia.misirli@gmail.com>
2019-05-02 11:33:39 +1000

    Add User + bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
-class User < ActiveRecord::Base
+
+class User 
   include Mongoid::Document
+  # must add the below code because we use mongodb, this gives us the "has_secure_password"
+  include ActiveModel::SecurePassword 
   has_secure_password
   field :email, type: String
   field :password_digest, type: String
+  validates :email, :password_digest, length: {minimum: 2}, allow_blank: false
 end
+
