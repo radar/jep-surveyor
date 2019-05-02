062cceaabb56417543c02a7239c536ff9163b748
Jody <jody.vandenschrick@cultureamp.com>
2019-05-02 17:21:47 +1000

    modify application to load .env file

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ require "rails"
   end
 end
 
-  
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -26,6 +25,7 @@ module Surveyor
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.autoload_paths += %W[#{config.root}/lib]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
