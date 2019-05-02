2f0d35f81ecfee50e05a3efd3a3f7b5aacd36891
Jody <jody.vandenschrick@cultureamp.com>
2019-05-02 17:19:22 +1000

    Modify to integrate the ENV variable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,23 @@
-require 'jwt'
-
 class User
-
   include Mongoid::Document
   include ActiveModel::SecurePassword
+
+  belongs_to :account
+
   field :email, type: String
   field :password_digest, type: String
   
   has_secure_password
   
-  
-    def private_key
-      'my$ecretK3y'
-    end
-    
-    def payload(user:)
-      {
-        email: email
-        id: id
-      }
-    end
-    
   def token
-    token = JWT.encode payload, private_key, 'HS256'
+    JWT.encode payload, ENV['PRIVATE_KEY'], 'HS256'
   end
-
-private
+  
+  private
+  def payload
+    {
+      email: email,
+      id: id
+    }
+  end
 end
