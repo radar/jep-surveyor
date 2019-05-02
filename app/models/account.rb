ff63b60265c2e3fe755de9449a7e1a1be3a7c378
Jody <jody.vandenschrick@cultureamp.com>
2019-05-02 17:17:44 +1000

    Modify models to integrate accounts DB connection

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,7 @@
 class Account
   include Mongoid::Document
+  has_many :survey
+  has_many :user
+  
   field :name, type: String
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,7 @@
 class Survey
   include Mongoid::Document
   has_many :rating_question, dependent: :destroy
+  belongs_to :account
   
   field :name, type: String
 end
