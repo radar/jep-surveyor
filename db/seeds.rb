f79936a4626cf0d62da2a14dcd2cd43e1e4736de
Kasia <kasia.misirli@gmail.com>
2019-05-03 11:52:22 +1000

    Add missing User data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,23 @@
 # Coles
 Account.create!([{ name:"Coles", id:"coles" }])
 Survey.create!([{ name:"Coles Survey", account_id:"coles",id:"coles_survey"}])
+User.create!([
+  { email:"kasia@gmail.com", password:"kasia", account_id:"coles"},
+  { email:"jake@gmail.com", password:"jake", account_id:"coles"},
+  { email:"caz@gmail.com", password:"caz", account_id:"coles"},
+  { email:"jody@gmail.com", password:"jody", account_id:"coles"}
+])
+
 
 # Culture Amp
 Account.create!([{ name:"Culture Amp", id:"cultureamp" }])
 Survey.create!([{ name:"Culture Amp Survey", account_id:"cultureamp",id:"cultureamp_survey"}])
-
+User.create!([
+  { email:"nick@gmail.com", password:"nick", account_id:"cultureamp"},
+  { email:"todd@gmail.com", password:"todd", account_id:"cultureamp"},
+  { email:"ryan@gmail.com", password:"ryan", account_id:"cultureamp"},
+  { email:"tanim@gmail.com", password:"tanim", account_id:"cultureamp"}
+])
 
 @coles_question_list = [
   ["How many days off do you need?","work","coles_survey"],
